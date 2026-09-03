2f6cbcecf96862d1866187ff21d8d86e9428eb8a
Atharv pratap singh <atharvsingh5678@gmail.com>
2026-09-03 22:12:28 +0530

    chore: remove mock auth forms, move consent form

--- apps/web/src/app/(public)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from '../../../features/auth/SignInForm';
-
-export default function SignInPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center px-6">
-      <SignInForm />
-    </main>
-  );
-}
